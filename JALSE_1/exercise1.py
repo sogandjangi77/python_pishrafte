c6ae22985ff3159391a9a05a92aac2d8516c7439
sogsnd jangi77 <sogand.jangi.7.army@gmali.com>
2024-09-28 16:57:37 +0330

    first commit

---
+++ JALSE_1/exercise1.py
@@ -0,0 +1,9 @@
+try:
+    age=int(input('please enter your age:'))
+    if age >=18
+    print('you can vote.')
+    else:
+        print('you can nat vote')
+        except ValueError:
+            print('pse entleaer a valid number")
+            
